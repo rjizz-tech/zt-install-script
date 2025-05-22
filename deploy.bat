5327a41ad5e94ba89d1b0c42b2d04ab5c6c1f04d
rjizz-tech <rj+github@rjplummer.com>
2025-05-22 18:48:05 +0800

    Create deploy.bat
    batch file wrapper

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+REM This batch file will run the SD-WAN deployment script with the necessary permissions.
+
+REM Prompt the user to ensure they run this as an administrator if they haven't already.
+echo Make sure you have right-clicked this .bat file and selected "Run as administrator".
+echo.
+pause
+echo.
+
+echo Launching the Installation and Configuration Script...
+powershell.exe -ExecutionPolicy Bypass -File ".\deploy-sd-wan.ps1"
+
+echo.
+echo The script has finished. You can review any messages above.
+pause
+@echo on
